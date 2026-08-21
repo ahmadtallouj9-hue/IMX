67c05854dfa7675f3a49cd6c9172c36550fb2ee9
Ahmad Tallouj <ahmadtallouj9@gmail.com>
2026-08-21 07:29:39 -0700

    Remove prisma generate from start - build handles it

--- start.sh
+++ start.sh
@@ -5,8 +5,5 @@ cd server
 echo "Waiting 10s for database to wake up..."
 sleep 10
 
-echo "Generating Prisma client..."
-npx prisma generate
-
 echo "Starting server..."
 exec node dist/src/server.js
