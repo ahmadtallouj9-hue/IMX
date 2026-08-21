07e8d0b87e83a4f9bafa3a45b8effe1e11717557
Ahmad Tallouj <ahmadtallouj9@gmail.com>
2026-08-21 15:20:24 -0700

    fix: Android APK with local notifications + notification button fix

--- web/android/app/src/main/java/app/cove/chat/MainActivity.java
+++ web/android/app/src/main/java/app/cove/chat/MainActivity.java
@@ -1,13 +1,5 @@
 package app.cove.chat;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.plugins.localnotifications.LocalNotifications;
 
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    registerPlugin(LocalNotifications.class);
-    super.onCreate(savedInstanceState);
-  }
-}
+public class MainActivity extends BridgeActivity {}
